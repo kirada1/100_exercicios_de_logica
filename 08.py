cac6267267a0be46e12d9c7ea49ddae091332903
kirada1 <matheusmtg16@gmail.com>
2025-09-30 21:20:58 -0300

    modifiquei o exercicio 08, adicionei outra maneira de fazer

--- 08.py
+++ 08.py
@@ -1,13 +1,23 @@
+import math
 # Peça um número e mostre o dobro, o triplo e a raiz quadrada.
-numero = 25
+numero = 24
 dobro = numero * 2
 tripo = numero * 3
 for i in range(numero):
     if i * i == numero:
         raiz = i
+        print(f"{i} ea raiz de {raiz}")
         break
     else:
-        print(f"{i} não e a raiz de {numero}")
+        print(f"{numero} não e uma raiz quadrada")
+        break
 
-print(f"""o dobro de {numero} e {dobro} o triplo e {tripo} e a raiz de {raiz}
-       e {i}""")
+# outra maneira de resolver
+for i in range(numero):
+    if i * i == numero:
+        raiz_quadrada = math.sqrt(numero)
+        print(f"a raiz quadrada de {numero} e {raiz_quadrada}")
+        break
+    else:
+        print(f"{numero} não tem raiz quadrada")
+        break
